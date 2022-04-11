821083c721ab5b0f81cf106e733098f09692ca62
Kenji-Wang <kenjiw360@gmail.com>
2022-04-11 11:03:53 -0700

    project setup

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+//Server port to listen to
+const PORT = process.env.PORT || 6666;
+
+//Importing server modules
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io")
 const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
+//HTTP server configuration
 const app = express();
-
-app.set("view engine","ejs");
+const server = http.createServer(app);
 app.use(helmet());
+app.use(cors());
+app.set("view engine","ejs");
 
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+//
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/CadmiumMB");
 
-server.listen(6666);
+//Web socket server configuration
+const io = socketio(server);
+
+//Opening server to internet
+server.listen(PORT, console.log(`Listening to port ${PORT}`));
